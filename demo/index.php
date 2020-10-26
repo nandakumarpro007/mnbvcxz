d01ff86a21c8c9a083497315256414f51a34d0d5
nandakumarpro007 <73435814+nandakumarpro007@users.noreply.github.com>
2020-10-26 20:58:06 +0530

    updated

--- demo/index.php
+++ demo/index.php
@@ -3,9 +3,9 @@
 
     $iap = new InstagramAutoPilot;
 
-    $accounts = array("girlslaying", "learnedfeeds", "pubity", "memes_you.need", "redditwanderes");
-    $tags = array("wholesomememes", "cutememes", "memes", "lmao", "memesdaily");
+    $accounts = array("girlslaying","learnedfeeds","pubity","memes_you.need","redditwanderes");
+    $tags = array("wholesomememes","cutememes","memes","lmao","memesdaily");
     $comment = "Check out my profile for all the best memes and feel good feeds!";
 
-    $iap->init("feelgoodfeeds", "nandakumarpro@007", true, true, false, $accounts, $tags, $comment);
+    $iap->init("feelgoodfeeds","nandakumarpro@007",true,true,false,$accounts,$tags,$comment);
 ?>
